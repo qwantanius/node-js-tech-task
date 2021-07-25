409f0a00ae362a885da1c09597e9e6b1b7e2a3d5
qwantanius <borshbrawl@gmail.com>
2021-07-25 17:17:12 +0300

    TODO method create DONE Check HTTP for API endpoints

--- app.js
+++ app.js
@@ -4,25 +4,29 @@ const bodyParser = require('body-parser');
 const app = express();
 
 
-
+//delete user
 app.delete('/del/:id', (req, res) => {
   let id = req.params.id;
   id = id !== undefined ? id :
     res.json("ID cannot be empty");
   user.del(id);
   res.json("OK");
-})
+});
 
+
+//get user
 app.get('/get/:id', (req, res) => {
   let id = req.params.id;
   res.json(user.get(id));
-})
+});
 
 
-
-
-
-
+//create user
+app.use(bodyParser.json());
+app.post('/create/', (req, res) => {
+  user.create(req.body);
+  res.json("OK");
+});
 
 
 app.listen(8080);
